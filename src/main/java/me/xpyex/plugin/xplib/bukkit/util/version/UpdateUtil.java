0f66b013851a32940205af881db2ac3e72813ce8
XPYEX <XPYEX0@163.com>
2024-06-25 02:27:24 +0800

    添加GitHub更新检测

--- src/main/java/me/xpyex/plugin/xplib/bukkit/util/version/UpdateUtil.java
+++ src/main/java/me/xpyex/plugin/xplib/bukkit/util/version/UpdateUtil.java
@@ -3,8 +3,11 @@ package me.xpyex.plugin.xplib.bukkit.util.version;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import me.xpyex.plugin.xplib.bukkit.XPLib;
 import me.xpyex.plugin.xplib.bukkit.api.Version;
 import me.xpyex.plugin.xplib.bukkit.util.RootUtil;
@@ -38,9 +41,44 @@ public class UpdateUtil extends RootUtil {
                 return name;
             }
         } catch (Throwable e) {
-            XPLib.getInstance().getLogger().warning("插件 " + plugin.getName() + " 获取更新失败");
+            XPLib.getInstance().getLogger().warning("插件 " + plugin.getName() + " 从 Gitee 获取更新失败");
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static String getUpdateFromGitHub(Plugin plugin) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL("https://api.github.com/repos/0XPYEX0/" + plugin.getName() + "/releases/latest").openConnection();
+
+            connection.setReadTimeout(10 * 1000);
+            connection.setRequestMethod("GET");
+            connection.connect();
+            if (connection.getResponseCode() != 200) {
+                throw new IOException("连接被服务器拒绝，访问码: " + connection.getResponseCode());
+            }
+            JsonObject result = GsonUtil.parseJsonObject(new String(readInputStream(connection.getInputStream()), StandardCharsets.UTF_8));
+            connection.disconnect();
+            String newVer = result.get("tag_name").getAsString().toLowerCase().replace("v", "");
+            if (new Version(newVer).compareTo(new Version(plugin.getDescription().getDescription())) > 0) {
+                return newVer;
+            }
+        } catch (IOException e) {
+            XPLib.getInstance().getLogger().warning("插件 " + plugin.getName() + " 从 GitHub 获取更新失败");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    private static byte[] readInputStream(InputStream inputStream) throws IOException {
+        try (ByteArrayOutputStream ba = new ByteArrayOutputStream(16384)) {
+            int nRead;
+            byte[] data = new byte[4096];
+            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+                ba.write(data, 0, nRead);
+            }
+            return ba.toByteArray();
+        }
     }
 }
